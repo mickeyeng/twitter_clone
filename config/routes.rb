8cdedb3cd0af0149164daf809404c760820b99e3
Mickey English <mickeyenglish@Mickeys-MacBook-Pro.local>
2017-07-02 02:09:59 +0100

    added panels and corrected page url's

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  get 'pages/home' => 'pages#home'
+  get 'home' => 'pages#home'
 
-  get 'pages/profile' => 'pages#profile'
+  get 'profile' => 'pages#profile'
 
-  get 'pages/explore' => 'pages#explore'
+  get 'explore' => 'pages#explore'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
